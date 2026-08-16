59d4cb080fca620e7d367dd14cd359360af53400
KDSealock <158773839+Arrhythmiagamer23@users.noreply.github.com>
2026-08-16 16:12:41 -0400

    Add Pusab font to service worker cache

--- game/sw.js
+++ game/sw.js
@@ -37,6 +37,7 @@ var version = "1.1.3",
     "fonts/Poppins-Regular.ttf",
     "fonts/Poppins-SemiBold.ttf",
     "fonts/Poppins-SemiBoldItalic.ttf",
+    "fonts/Pusab.otf",
 
     // favicon
     "./favicon.ico",
